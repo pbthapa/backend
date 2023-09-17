e8ad5899a38ab9e04c7511a8919630896630a24b
pbthapa <ptpbthapa24@gmail.com>
2023-09-17 12:19:20 +0200

    adding one service

---
+++ weather-service/weather-service-rest/src/main/java/com/app/pt/backend/weather/WeatherApplication.java
@@ -0,0 +1,11 @@
+package com.app.pt.backend.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class WeatherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApplication.class, args);
+    }
+}
